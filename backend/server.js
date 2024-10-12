acb3287cddca0ad8431a4d2ecc408d6e67089ade
Rad129 <rad129ratul>
2024-10-12 15:25:39 +0600

    videofile7done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js'
 
 const port = process.env.PORT || 5000;
 
@@ -11,11 +13,17 @@ connectDB();
 
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
+
 app.get('/', (req, res) => {
     res.send('API is running......')
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,4 +20,4 @@ const App = () => {
 
 export default App
 
-//file 7
+//file 7 video 7 problem
